a97c51d8bbb54eb5649402c1b77df4cb05a4207c
mtempleton94 <mitchelltempleton94@gmail.com>
2018-08-29 16:00:13 +0930

    Added grid background to the designer pane

--- DatabaseDesigner/src/application/DesignerPane.java
+++ DatabaseDesigner/src/application/DesignerPane.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Line;
 
 /**
 * <h1>Designer Pane</h1>
@@ -14,7 +15,31 @@ import javafx.scene.layout.Pane;
 public class DesignerPane extends Pane {
 	
 	public DesignerPane() {
-		this.setStyle("-fx-background-color: yellow;");
+		this.setStyle("-fx-background-color: #3d3e3f;");
+
+		// add a grid to the pane
+		double paneMaxSize = 2000;
+		for(int i = 0; i <= paneMaxSize; i += 20) {
+
+			// vertical line
+			Line verticalLine = new Line();
+			verticalLine.setStartX(i);
+			verticalLine.setStartY(0.0);
+			verticalLine.setEndX(i);
+			verticalLine.setEndY(paneMaxSize);
+			verticalLine.setStyle("-fx-stroke: #4b4d4f;");
+			this.getChildren().add(verticalLine);
+
+			// horizontal line
+			Line horizontalLine = new Line();
+			horizontalLine.setStartX(0.0);
+			horizontalLine.setStartY(i);
+			horizontalLine.setEndX(paneMaxSize);
+			horizontalLine.setEndY(i);
+			horizontalLine.setStyle("-fx-stroke: #4b4d4f;");
+			this.getChildren().add(horizontalLine);
+		}
+
 	}
 	
 	/**
