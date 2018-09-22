e8874f5e38be948266bcfd39ddc8fed568d0cdcb
mtempleton94 <mitchelltempleton94@gmail.com>
2018-09-22 20:04:03 +0930

    Added enumerated types for field key types and field data types.

---
+++ DatabaseDesigner/src/application/DataType.java
@@ -0,0 +1,38 @@
+package application;
+
+/**
+ *  Types of data to be used for table fields
+ */
+public enum DataType {
+	BIGINT,
+	BINARY,
+	BIT,
+	CHAR,
+	DATE,
+	DATETIME,
+	DATETIME2,
+	DATETIMEOFFSET,
+	DECIMAL,
+	FLOAT,
+	IMAGE,
+	INT,
+	MONEY,
+	NCHAR,
+	NTEXT,
+	NUMERIC,
+	NVARCHAR,
+	REAL,
+	ROWVERSION, 
+	SMALLDATETIME,
+	SMALLINT,
+	SMALLMONEY,
+	SQL_VARIANT,
+	TEXT,
+	TIME,
+	TIMESTAMP,
+	TINYINT,
+	UNIQUEIDENTIFIER,
+	VARBINARY,
+	VARCHAR,
+	XML
+}
---
+++ DatabaseDesigner/src/application/KeyType.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ *  Types of key to be used for table fields
+ */
+public enum KeyType {
+	SUPER,
+	CANDIDATE,
+	PRIMARY,
+	ALTERNATE,
+	COMPOSITE,
+	UNIQUE,
+	FOREIGN	
+}
